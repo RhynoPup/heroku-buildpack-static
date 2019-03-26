640af6454345f58d527a44605e4277cf2a250cdd
Ryan Mohrman <ryan.mohrman@gmail.com>
2019-03-26 00:18:47 -0600

    updated nginx.conf.erb to include semi_private configuration for the location block and updated nginx_config.erb to reference new semi_private variable

--- scripts/config/lib/nginx_config.rb
+++ scripts/config/lib/nginx_config.rb
@@ -8,6 +8,7 @@ class NginxConfig
     encoding: "UTF-8",
     clean_urls: false,
     https_only: false,
+    semi_private: false,
     worker_connections: 512,
     resolver: "8.8.8.8",
     logging: {
@@ -44,6 +45,7 @@ class NginxConfig
 
     json["clean_urls"] ||= DEFAULT[:clean_urls]
     json["https_only"] ||= DEFAULT[:https_only]
+    json["semi_private"] ||= DEFAULT[:semi_private]
 
     json["routes"] ||= {}
     json["routes"] = NginxConfigUtil.parse_routes(json["routes"])
